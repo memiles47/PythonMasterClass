ba613ed8b2b1cac6336304791d175dfae733ed05
Michael Miles <michael@miles-me.net>
2019-10-20 14:10:37 -0500

    Multiple Lessons
    New .py files for each

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -4,3 +4,11 @@ print(1 + 2)
 print(7 * 6)
 print()
 print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = "Michael"
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting + ' ' + name)
