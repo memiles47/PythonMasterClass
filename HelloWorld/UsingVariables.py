c2d20352ba15c4a2889ac826939ff54be9c7f3cd
Michael Miles <michael@miles-me.net>
2019-10-21 03:58:29 -0500

    Continuing edudation with Udemy

--- HelloWorld/UsingVariables.py
+++ HelloWorld/UsingVariables.py
@@ -1,1 +1,47 @@
 __author__ = 'dev'
+# greeting = "Michael"
+# _myName = "Mike"
+# Mike57 = "Good"
+# # 1Mike = "Bad"
+# Mike57_was_56 = "Hello"
+# Greeting = "There"
+# print(Mike57_was_56 + " " + greeting)
+#
+# age = 24
+# print(age)
+#
+# print(greeting + age)
+
+# Integers and order of operations
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+for i in range(1, a//b):
+    print(i)
+print()
+print(a + b / 3 - 4 * 12)
+
+# Order of Precedence examples
+print(8 / 2 * 3)
+print(8 * 3 / 2)
+
+print((((a + b) / 3) - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+# String variables
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+
+
+
