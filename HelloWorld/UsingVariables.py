690111419d5203739736394348cbd9f919b9ad59
Michael Miles <michael@miles-me.net>
2019-10-22 04:04:08 -0500

    Course Continuation

--- HelloWorld/UsingVariables.py
+++ HelloWorld/UsingVariables.py
@@ -41,7 +41,35 @@ print(e * 12)
 # String variables
 parrot = "Norwegian Blue"
 print(parrot)
+print(parrot[0])
 print(parrot[3])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+
+wholeNumbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(wholeNumbers[::3])
+
+string1 = "He's "
+string2 = "probably "
+print(string1 + string2)
+print("He's " "probably " "pining")
+print("Hello " * 5)
+
+today = "Monday"
+print("day" in today)
+print("Mon" in today)  # Case Sensitive
+print("Thur" in today)
+print("parrot" in "fjord")
+
+
 
 
 
