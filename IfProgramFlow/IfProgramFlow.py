0848040c265f5d8f16af16b529354f9c6c50a922
Michael Miles <michael@miles-me.net>
2019-10-27 18:16:55 -0500

    Update IfProgramFlow.py
    Using 'and' within an if statement

--- IfProgramFlow/IfProgramFlow.py
+++ IfProgramFlow/IfProgramFlow.py
@@ -8,36 +8,43 @@ __author__ = 'Michael E Miles'
 #     print('Please put an " in the box.')
 # else:
 #     print('Please com back in {} years {}.'.format(18 - age, name))
-print('Please guess a number between 1 and 10: ')
-guess = int(input())
-
-if guess < 5:
-    print('Please guess higher')
-    guess = int(input())
-    if guess == 5:
-        print('Well done, you guessed it')
-    else:
-        print('Sorry, you have not guessed correctly.')
-elif guess > 5:
-    print('Please guess lower')
-    guess = int(input())
-    if guess == 5:
-        print('Well done, you guessed it')
-    else:
-        print('Sorry, you have not guessed correctly')
-else:
-    print('You got it the first time')
+# print('Please guess a number between 1 and 10: ')
+# guess = int(input())
+#
+# if guess < 5:
+#     print('Please guess higher')
+#     guess = int(input())
+#     if guess == 5:
+#         print('Well done, you guessed it')
+#     else:
+#         print('Sorry, you have not guessed correctly.')
+# elif guess > 5:
+#     print('Please guess lower')
+#     guess = int(input())
+#     if guess == 5:
+#         print('Well done, you guessed it')
+#     else:
+#         print('Sorry, you have not guessed correctly')
+# else:
+#     print('You got it the first time')
 
 # More compact code
-if guess != 5:
-    if guess < 5:
-        print('Please guess higher')
-    else:
-        print('Please guess lower')
-    guess = int(input())
-    if guess == 5:
-        print('Well done, you guessed it')
-    else:
-        print('Sorry you have not guessed correctly')
-else:
-    print('You got it the first time')
+# if guess != 5:
+#     if guess < 5:
+#         print('Please guess higher')
+#     else:
+#         print('Please guess lower')
+#     guess = int(input())
+#     if guess == 5:
+#         print('Well done, you guessed it')
+#     else:
+#         print('Sorry you have not guessed correctly')
+# else:
+#     print('You got it the first time')
+
+# More complex if control code
+age = int(input('How old are you? '))
+
+# if (age >= 16) and (age <= 65):
+if 16 <= age <= 65:
+    print('Have a good day at work')
