55b1eca5a706343ad215997593a31f4b85165630
Michael Miles <michael@miles-me.net>
2019-11-19 04:25:15 -0600

    Continue lessons

--- HelloWorld/ForLoops.py
+++ HelloWorld/ForLoops.py
@@ -49,3 +49,8 @@ for i in range(1, 13):
     for j in range(1, 13):
         print('{1} times {0} is {2}'.format(i, j, i*j))
     print('=================================')
+
+print('\n\n')
+for i in range(0, 100):
+    if i % 7 == 0:
+        print(i)
