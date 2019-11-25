3b3379e2b243c56b45ef829a776fc6baef7fdfc1
Michael Miles <michael@miles-me.net>
2019-11-25 04:17:48 -0600

    Create WhileLoops.py

---
+++ HelloWorld/WhileLoops.py
@@ -0,0 +1,10 @@
+__author__ = 'Michael E Miles'
+# import PyPDF2 as p2
+
+# for i in range(10):
+#     print('i is now: {}'.format(i))
+
+i = 0
+while i < 10:
+    print('i is now: {}'.format(i))
+    i += 1
