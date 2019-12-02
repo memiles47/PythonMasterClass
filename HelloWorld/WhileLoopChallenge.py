25489acaa6ad0fb7b4961c904d5941d3ae9448e2
Michael Miles <michael@miles-me.net>
2019-12-02 09:01:47 -0600

    Add While Challenge

--- HelloWorld/WhileLoopChallenge.py
+++ HelloWorld/WhileLoopChallenge.py
@@ -5,8 +5,18 @@ import random
 highest = 10
 answer = random.randint(1, highest)
 
-print("Please guess a number between 1 and {}: ".format(highest), end="")
+print("Please guess a number between 1 and {} (enter 0 'zero' to quit: ".format(highest), end="")
 guess = int(input())
 
 while guess != answer:
-    print("Guess again")
+    if guess == 0:
+        print("You Chicken!")
+        break
+    if guess < answer:
+        print("Please guess higher: ", end="")
+        guess = int(input())
+    else:
+        print("Please guess lower: ", end="")
+        guess = int(input())
+else:
+    print("Good Job! The answer is {}".format(guess))
